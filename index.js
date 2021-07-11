cc7ace8af12b10cb9c20d675ea6eaef95d00da3a
Ishtiaq Ahammed <isah2080@ymail.com>
2021-07-11 09:48:54 +0600

    added save tab button and made it to be work. now it's ready to deploy

--- index.js
+++ index.js
@@ -5,39 +5,29 @@ const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 // store the delete button
 const deleteBtn = document.getElementById("delete-btn")
-const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+const tabBtn = document.getElementById("tab-btn")
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
-    renderLeads()
+    render(myLeads)
 }
 
-// listen for double click on delete button.
-// when clicked, clear localStorage, myLeads and DOM
+tabBtn.addEventListener("click", function () {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+        //had to add permission for chrome extension in the manifest.json file
+    })
 
-deleteBtn.addEventListener("dblclick", function() {
-    localStorage.clear()
-    myLeads = []
-    renderLeads()
-})
-
-inputBtn.addEventListener("click", function () {
-    myLeads.push(inputEl.value)
-    // Two. Call the renderLeads() function
-
-    // now clear out the input field
-    inputEl.value = ""
-
-    // save the myLeads array to localStorage
-    localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    renderLeads()
 })
 // One. Wrap the code below in a renderLeads() function
-function renderLeads() {
+function render(leads) {
     // 1. create a variable, listItems, to hold all the HTML for the list items
     // Assign it to an empty array string to begin with
     let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
+    for (let i = 0; i < leads.length; i++) {
         // 2. Add the item to the listItems variable instead of the ulEl.innerHTML
 
 
@@ -45,8 +35,8 @@ function renderLeads() {
         // make the link open in a new tab
         listItems += `
         <li>
-              <a target='_blank' href='${myLeads[i]}'>
-                ${myLeads[i]}
+              <a target='_blank' href='${leads[i]}'>
+                ${leads[i]}
               </a>
         </li>
         `
@@ -60,4 +50,26 @@ function renderLeads() {
     // 3. Render the listItems inside the unordered list using ulEl.innerHTML
     ulEl.innerHTML = listItems
 }
+
+// listen for double click on delete button.
+// when clicked, clear localStorage, myLeads and DOM
+
+deleteBtn.addEventListener("dblclick", function () {
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
+
+inputBtn.addEventListener("click", function () {
+    myLeads.push(inputEl.value)
+    // Two. Call the renderLeads() function
+
+    // now clear out the input field
+    inputEl.value = ""
+
+    // save the myLeads array to localStorage
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads)
+})
+
 
